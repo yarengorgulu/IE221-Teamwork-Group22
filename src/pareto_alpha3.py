fddd0fea0c82770b7e2415fd860107b30d031ffd
yarengorgulu <yrngorgulu638@gmail.com>
2026-01-18 20:56:22 +0300

    Update pareto_alpha3.py

--- src/pareto_alpha3.py
+++ src/pareto_alpha3.py
@@ -11,18 +11,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy import stats
 
-# --------------------------------------------------
-# THEORETICAL MOMENTS
-# --------------------------------------------------
+
 alpha = 3
 x_m = 1
 
 mu = (alpha * x_m) / (alpha - 1)          # E[X] = 3 / 2
 sigma = np.sqrt((alpha * x_m**2) / ((alpha - 1)**2 * (alpha - 2)))  # sqrt(3/4)
 
-# --------------------------------------------------
-# SLLN ANALYSIS
-# --------------------------------------------------
+
 n = 10000
 samples = np.random.pareto(alpha, size=n) + x_m
 
@@ -38,9 +34,6 @@ plt.legend()
 plt.savefig("../results/pareto_alpha3/slln.png")
 plt.close()
 
-# --------------------------------------------------
-# CLT ANALYSIS
-# --------------------------------------------------
 n_values = [2, 5, 10, 30, 50, 100]
 m = 1000
 
@@ -53,7 +46,7 @@ for n in n_values:
 
     means = np.array(means)
 
-    # Standardization (mean and variance are finite)
+ 
     Z = (means - mu) / (sigma / np.sqrt(n))
 
     # Histogram
@@ -72,9 +65,4 @@ for n in n_values:
     plt.savefig(f"../results/pareto_alpha3/clt_qq_n{n}.png")
     plt.close()
 
-# --------------------------------------------------
-# OBSERVATION
-# --------------------------------------------------
-# For Pareto(alpha=3), both SLLN and CLT hold since the mean and variance
-# are finite. However, convergence is significantly slower compared to
-# Uniform and Exponential distributions due to the heavy-tailed nature.
+
